9e3c4cbbc3888d7c9d2aa05de6ba71c672e2b0cb
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-03-08 12:10:31 +0700

    Upfdatw : Tambahan

--- BotDcLms.py
+++ BotDcLms.py
@@ -9,21 +9,22 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 @bot.event
 async def on_ready():
     print('Bot is ready.')
 
 @bot.command()
 async def cek_tugas(ctx):
-    url = 'https://slc.polinema.ac.id/spada/'
+    url = 'https://siakad.polinema.ac.id/beranda'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     # Proses parsing tugas dari halaman Siakad
     # Contoh: tugas = soup.find('div', class_='tugas').text
-    tugas = "Contoh tugas baru: Pertemuan 1: Perkenalan Developer - Bahasa Indonesia"
+    tugas = "Contoh tugas baru: Pertemuan 1: Perkenalan Developer - Bahasa Indonesia\n"
+    tugas += "Jangan Lupa dikerjakan, Jangan ditunda-tunda\n"
+    tugas += "Deadline: 10 Maret 2024 (Contoh deadline)"
     if tugas:
-        await ctx.send(f'Ada tugas baru: {tugas}')
+        await ctx.send(f'Ada tugas baru:\n{tugas}')
     else:
         await ctx.send('Tidak ada tugas baru.')
 
